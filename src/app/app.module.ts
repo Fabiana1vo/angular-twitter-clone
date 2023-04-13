bb5f10e581b424033618b07255a555ffa4523924
Fabiana1vo <fabianasouzaivo@icloud.com>
2023-04-12 21:45:33 -0300

    meu primeiro commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {  FormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostComponent } from './components/post/post.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NewPostComponent } from './components/new-post/new-post.component';
+import { TweetComponent } from './components/tweet/tweet.component';
+import { PagesComponent } from './components/pages/pages.component';
+import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostComponent,
+    HeaderComponent,
+    NewPostComponent,
+    TweetComponent,
+    PagesComponent,
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
